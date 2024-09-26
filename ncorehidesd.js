38df95f8b526705d4ac6d8b47235271886e19324
morpheus133 <morpheus1333@gmail.com>
2024-09-27 00:07:09 +0200

    Update ncorehidesd.js

--- ncorehidesd.js
+++ ncorehidesd.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name           ncoreHideSD
-// @namespace      https://github.com/morpheus133/ncoreHideSD/new/master
-// @updateURL   https://raw.githubusercontent.com/morpheus133/ncoreHideSD/refs/heads/master/ncorehidesd.js
-// @downloadURL https://raw.githubusercontent.com/morpheus133/ncoreHideSD/refs/heads/master/ncorehidesd.js
-// @description    Ncore-on eltünteti az SD tartalmakat a keresőből
+// @namespace      https://github.com/morpheus133/ncoreHideSD
+// @updateURL      https://raw.githubusercontent.com/morpheus133/ncoreHideSD/refs/heads/master/ncorehidesd.js
+// @downloadURL    https://raw.githubusercontent.com/morpheus133/ncoreHideSD/refs/heads/master/ncorehidesd.js
+// @description    Ncore-on eltünteti az SD tartalmakat a kereső mezőből
 // @icon           https://static.ncore.pro/styles/ncore.ico
 // @include        /^https?://ncore\.(cc|pro)\/.*/
 // @author         Morpheus133
 // @copyright      (C) 2024  Morpheus133
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-// @version        1.1
+// @version        1.2
 // @grant          GM_getValue
 // @grant          GM_setValue
 // @grant          GM_registerMenuCommand
@@ -18,17 +18,8 @@
 // ==/UserScript==
 
 
-// GM_addStyle('input.g_mehet { display: none; }');
+var tbl=document.getElementsByTagName("table")[1];
 
-
-//var fstTable    = document.querySelector ("table_torrcat");
-
-
-//fstTable.rows[2].cells[0].textContent = "*changed*";
-
-var tbl=document.getElementsByTagName("table")[1]; //get the first TABLE
-
-//tbl.rows[0].cells[1].width = 60;
 tbl.rows[0].deleteCell(1);
 tbl.rows[0].deleteCell(1);
 tbl.rows[0].deleteCell(1);
@@ -52,7 +43,7 @@ tbl.rows[0].insertCell();
 tbl.rows[0].insertCell();
 tbl.rows[0].insertCell();
 
-tbl=document.getElementsByTagName("table")[2]; //get the first TABLE
+tbl=document.getElementsByTagName("table")[2];
 tbl.rows[0].deleteCell(1);
 tbl.rows[0].deleteCell(1);
 tbl.rows[0].deleteCell(1);
@@ -69,9 +60,6 @@ tbl.rows[0].insertCell();
 tbl.rows[0].insertCell();
 tbl.rows[0].insertCell();
 tbl.rows[0].insertCell();
-
-
-
 
 
 
