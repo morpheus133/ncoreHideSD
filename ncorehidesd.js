f1bbc10497acb9510ada79c408d33305f365a0fb
morpheus133 <morpheus1333@gmail.com>
2024-09-27 00:03:54 +0200

    Update ncorehidesd.js

--- ncorehidesd.js
+++ ncorehidesd.js
@@ -9,7 +9,7 @@
 // @author         Morpheus133
 // @copyright      (C) 2024  Morpheus133
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-// @version        1.0
+// @version        1.1
 // @grant          GM_getValue
 // @grant          GM_setValue
 // @grant          GM_registerMenuCommand
